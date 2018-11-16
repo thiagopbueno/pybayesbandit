5412a2c8385e91472d69b891f31a949e53f0cb16
Thiago P. Bueno <thiago.pbueno@gmail.com>
2018-11-16 11:11:04 -0500

    Document BetaBernoulli reward and sample methods

--- pybayesbandit/mdp/beta_bernoulli.py
+++ pybayesbandit/mdp/beta_bernoulli.py
@@ -26,12 +26,28 @@ class BetaBernoulliMDP(BeliefMDP):
 
     @property
     def start(self):
+        '''Uniform prior'''
         return tuple([(1, 1)] * self.actions)
 
     def sample(self, belief, action):
+        '''
+        Sample from belief-state transition.
+        '''
         probs, next_beliefs = zip(*self.transition(belief, action))
         i = np.squeeze(np.where(np.random.multinomial(1, probs) == 1))
         return next_beliefs[i]
+
+    # def sample(self, belief, action):
+    #     '''
+    #     Sample bandit from posterior and then sample reward and
+    #     return corresponding next belief-state
+    #     '''
+    #     alpha, beta = belief[action]
+    #     theta = np.random.beta(alpha, beta)
+    #     r = int(np.random.sample() >= theta)
+    #     next_belief = tuple((params[0] + r, params[1] + 1 - r) if i == action else params \
+    #             for i, params in enumerate(belief))
+    #     return next_belief
 
     def transition(self, belief, action):
         alpha, beta = belief[action]
@@ -46,10 +62,26 @@ class BetaBernoulliMDP(BeliefMDP):
         return probs_and_next_beliefs
 
     # def reward(self, belief, action, next_belief):
+    #     '''Expected Value of Information (EVOI)'''
     #     eu_belief = max(alpha / (alpha + beta) for alpha, beta in belief)
     #     eu_next_belief = max(alpha / (alpha + beta) for alpha, beta in next_belief)
     #     evof = eu_next_belief - eu_belief
     #     return evof
 
+    # def reward(self, belief, action, next_belief):
+    #     '''Maximum Expeted Utility (MEU[b_t])'''
+    #     return max(alpha / (alpha + beta) for alpha, beta in belief)
+
     def reward(self, belief, action, next_belief):
-        return max(alpha / (alpha + beta) for alpha, beta in belief)
+        '''Expected Utility (EU[b_t, a_t])'''
+        alpha, beta = belief[action]
+        return alpha / (alpha + beta)
+
+    # def reward(self, belief, action, next_belief):
+    #     '''Beta/Bernoulli payoff'''
+    #     alpha, beta = belief[action]
+    #     next_alpha, next_beta = next_belief[action]
+    #     if next_alpha - alpha == 1 and next_beta - beta == 0:
+    #         return 1
+    #     if next_alpha - alpha == 0 and next_beta - beta == 1:
+    #         return 0
